790ee68e21b7468a2c6f62f4f4a7c9f5f4b1a7e5
Jardelson Feijao <99224638+jardelsonfeijao@users.noreply.github.com>
2022-09-20 11:56:15 -0300

    PROJETO API com Express-03

--- app/servidor.js
+++ app/servidor.js
@@ -2,8 +2,11 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
-
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos()) // Converter para JSON
@@ -14,10 +17,24 @@ app.get('/produtos/:id', (req, res, next) => {
 })
 
 app.post('/produtos', (req, res, next) => {
-    const  produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
         preco: req.body.preco
     })
+    res.send(produto)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
     res.send(produto)
 })
 
